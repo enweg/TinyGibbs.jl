baa48f0c544c4f02089d81867718b274d301ec4d
Enrico Wegner <wegner1996@gmail.com>
2023-04-04 16:24:45 +0200

    first prototype works only if first draw is independent of all others

--- src/TinyGibbs.jl
+++ src/TinyGibbs.jl
@@ -1,5 +1,85 @@
 module TinyGibbs
+using AbstractMCMC
+using Random
+using MacroTools
+using Distributions
 
-# Write your package code here.
+# The types here should be Val{GibbsModelName}
+struct TinyGibbsSampler <: AbstractMCMC.AbstractSampler 
+    draw::Function
+end 
+struct TinyGibbsModel <: AbstractMCMC.AbstractModel 
+    initial_values::NamedTuple
+end
+
+# Need a macro that takes in a description of the sampling steps
+# and creates a sample method that extends the sample method from 
+# AbstractMCMC
+
+function AbstractMCMC.step(
+    rng::Random.AbstractRNG, 
+    model::TinyGibbsModel, 
+    sampler::TinyGibbsSampler; 
+    kwargs...
+)
+    v = sampler.draw(rng, model.initial_values)
+    return v, v
+end
+
+function AbstractMCMC.step(
+    rng::Random.AbstractRNG, 
+    model::TinyGibbsModel, 
+    sampler::TinyGibbsSampler,
+    state; 
+    kwargs...
+)
+    v = sampler.draw(rng, state)
+    return v, v
+end
+
+
+macro tiny_gibbs(expr)
+    return esc(tiny_gibbs(expr))
+end
+function tiny_gibbs(expr)
+    def = MacroTools.splitdef(expr)
+    # println(def[:body])
+    def[:body] = MacroTools.postwalk(def[:body]) do sub_expr
+        if MacroTools.@capture(sub_expr, var_ ~ dist_)
+            return :($var = new_state[$(Meta.quot(var))] = rand(rng, $dist))
+        else
+            return sub_expr
+        end
+    end
+    pushfirst!(def[:body].args, :(new_state = deepcopy(state)))
+    push!(def[:body].args, :(return new_state))
+    def[:args] = vcat([:(rng::Random.AbstractRNG), :(state::Dict)], def[:args])
+    return MacroTools.combinedef(def)
+end
 
 end
+
+
+
+
+@tiny_gibbs function m()
+    a ~ Normal(b, z^2)
+    b ~ Normal(a/2, 1)
+    z ~ Gamma(a^2, b^2)
+end
+using StableRNGs
+rng = StableRNG(123)
+state = Dict(:a => 1.0, :b => 1.0, :z => 1.0)
+state = m(rng, state)
+
+
+# The above should become
+function m(rng, state)
+    new_state = deepcopy(state)
+    new_state[:a] = rand(rng, Normal(new_state[:b], new_state[:z]^2))
+    new_state[:b] = rand(rng, Normal(new_state[:a]/2, 1))
+    new_state[:z] = rand(rng, Gamma(new_state[:a]^2, new_state[:b]^2))
+    return new_state
+end
+
+
