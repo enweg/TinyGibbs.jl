254d473db59fa60a1b228af3a7a1db7470ad8a27
Enrico Wegner <wegner1996@gmail.com>
2023-04-04 17:09:13 +0200

    first prototype of macro working

--- src/TinyGibbs.jl
+++ src/TinyGibbs.jl
@@ -43,18 +43,39 @@ macro tiny_gibbs(expr)
 end
 function tiny_gibbs(expr)
     def = MacroTools.splitdef(expr)
-    # println(def[:body])
+    vars = _collect_vars(def[:body])
     def[:body] = MacroTools.postwalk(def[:body]) do sub_expr
         if MacroTools.@capture(sub_expr, var_ ~ dist_)
-            return :($var = new_state[$(Meta.quot(var))] = rand(rng, $dist))
+            return :($var = rand(rng, $dist))
         else
             return sub_expr
         end
     end
+    def[:body] = _replace_variables(def[:body], vars)
     pushfirst!(def[:body].args, :(new_state = deepcopy(state)))
     push!(def[:body].args, :(return new_state))
     def[:args] = vcat([:(rng::Random.AbstractRNG), :(state::Dict)], def[:args])
     return MacroTools.combinedef(def)
+end
+function _collect_vars(body)
+    vars = []
+    MacroTools.postwalk(body) do sub_expr
+        if MacroTools.@capture(sub_expr, var_ ~ dist_)
+            push!(vars, var)
+        end
+        return sub_expr
+    end
+    return vars
+end
+function _replace_variables(expr, vars)
+    expr = MacroTools.postwalk(expr) do sub_expr
+        if sub_expr in vars 
+            return :(new_state[$(Meta.quot(sub_expr))])
+        else
+            return sub_expr
+        end
+    end
+    return expr
 end
 
 end
@@ -78,24 +99,20 @@ test_state = m_test(StableRNG(123), state)
 @test test_state == macro_state
 
 
-@tiny_gibbs function m()
+@tiny_gibbs function m2()
     a ~ Normal(b, z^2)
     b ~ Normal(a/2, 1)
     z ~ Gamma(a^2, b^2)
 end
-using StableRNGs
-rng = StableRNG(123)
-state = Dict(:a => 1.0, :b => 1.0, :z => 1.0)
-state = m(rng, state)
-
-
-# The above should become
-function m(rng, state)
+function m2_test(rng, state)
     new_state = deepcopy(state)
     new_state[:a] = rand(rng, Normal(new_state[:b], new_state[:z]^2))
     new_state[:b] = rand(rng, Normal(new_state[:a]/2, 1))
     new_state[:z] = rand(rng, Gamma(new_state[:a]^2, new_state[:b]^2))
     return new_state
 end
-
+state = Dict(:a => 1.0, :b => 0.5, :z => 10.0)
+macro_state = m2(StableRNG(123), state)
+test_state = m2_test(StableRNG(123), state)
+@test test_state == macro_state
 
